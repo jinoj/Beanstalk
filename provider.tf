906cf4b38540dc9df130a42de4e04fce79b22c73
jinoj <jinojoe@gmail.com>
2020-05-02 11:41:25 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  secret_key = "Nc+uWXZkOmNIEkhS/ArQseYtJhJbDquO/PJkNlo0"
-  access_key = "AKIAVCG6XK6QRUVHDJ7W"
+  #secret_key = ""
+  #access_key = ""
 }
